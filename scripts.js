9e3a6f1dfddb42624cda5075cfa42260ba3f163c
starrymeadows <aliharris.star@gmail.com>
2022-08-04 20:59:57 -0400

    Add capacity to resize grid

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 const container = document.querySelector('.container');
 
+let gridSize = 25;
+
 function buildGrid() {
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < gridSize; i++) {
         const row = document.createElement('row');
         row.className = "row";
         container.appendChild(row);
-        for (j = 0; j < 16; j++) {
+        for (j = 0; j < gridSize; j++) {
             const cell = document.createElement('div');
             cell.className = "cell";
             cell.addEventListener('mouseover', () => {
@@ -17,3 +19,16 @@ function buildGrid() {
 }
 
 buildGrid();
+resizeCells(gridSize);
+console.log(cellSize);
+
+// determine cell size
+function resizeCells(gridSize) {
+    cellSize = (500 / gridSize).toFixed(1) + `px`;
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.style.height = cellSize;
+        cell.style.width = cellSize;
+    });
+}
